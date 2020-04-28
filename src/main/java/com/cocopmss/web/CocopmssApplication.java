f23c133c247d92a11c2f2b846c5b53798e808394
Bit <iamcocopams@gmail.com>
2020-04-28 13:01:54 +0900

    200428

---
+++ src/main/java/com/cocopmss/web/CocopmssApplication.java
@@ -0,0 +1,13 @@
+package com.cocopmss.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocopmssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocopmssApplication.class, args);
+	}
+
+}
